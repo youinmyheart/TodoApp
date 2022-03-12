107ce0b2f51a2edfdaa9e45f215ed63520aee5fa
Phong Vu <tuanphong1081985@gmail.com>
2022-03-12 09:24:02 +0700

    init project

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+    }
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
+}
